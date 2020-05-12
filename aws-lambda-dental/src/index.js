80103d5abdbad8c239a71b0b2fb0f18ed67fbed3
ddam40 <craftyworks@gmail.com>
2020-05-12 22:36:07 +0900

    :bang: ㄱㅁ 덴탈마스크

---
+++ aws-lambda-dental/src/index.js
@@ -0,0 +1,98 @@
+const chromium = require('chrome-aws-lambda');
+const naver = require('./naver')
+const S3Client = require("aws-sdk/clients/s3");
+
+const browserOptions = async () => {
+  console.log('stage: ', process.env.stage)
+  if (process.env.stage === 'prod') {
+    return {
+      executablePath: await chromium.executablePath,
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      headless: chromium.headless,
+    }
+  } else {
+    return {
+      headless: false,
+      defaultViewport: {width: 1900, height: 900},
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ],
+      ignoreDefaultArgs: ['--disable-extensions']
+    }
+  }
+}
+
+const getTime = () => new Date().toISOString().slice(0, 19).replace(/[^0-9]/g, '')
+
+/**
+ * 현재 스크린샷을 저장하여 S3 Bucket 에 업로드
+ */
+const saveScreenshot = async(page, fileName) => {
+  const {s3_region, s3_bucket} = process.env
+  const s3 = new S3Client({ region: s3_region });
+  const buffer = await page.screenshot()
+  let result = await s3.upload({
+    Bucket: s3_bucket,
+    Key: `${fileName}.png`,
+    Body: buffer,
+    ContentType: 'image/png',
+  }).promise()
+  console.log('upload screenshot ', result.Bucket, result.Key)
+
+  const html = await page.content();
+  result = await s3.upload({
+    Bucket: s3_bucket,
+    Key: `${fileName}.html`,
+    Body: html,
+    ContentType: 'text/html',
+  }).promise()
+  console.log('upload html ', result.Bucket, result.Key)
+}
+
+/**
+ * 메인 람다 펑션
+ */
+module.exports.macro = async (event, context, callback) => {
+  const {naver_id, naver_password} = process.env
+
+  let browser = null
+  let page = null
+  let error = null
+  let success = false
+  try {
+    browser = await chromium.puppeteer.launch(await browserOptions())
+    page = await browser.newPage()
+    // Login
+    if (await naver.login(page, naver_id, naver_password)) {
+      let count = Number(event.count) || 100
+      while (!success && count-- > 0) {
+        // Move Product Page
+        await page.goto(naver.product)
+        if (await page.$('div.not_goods')) {
+          console.log('%d sold out!', count)
+        } else {
+          console.log('%d for sale', count)
+          success = await naver.buyProduct(page)
+          await saveScreenshot(page, `${getTime()}_${success}`)
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err)
+    await saveScreenshot(page, `${getTime()}_error`)
+    error = err
+  } finally {
+    if (page !== null) {
+      for (let page of await browser.pages()) {
+        await page.close({
+          'runBeforeUnload': true
+        })
+      }
+      await browser.disconnect()
+      await browser.close()
+    }
+  }
+  callback(error, {success})
+}
---
+++ aws-lambda-dental/src/naver.js
@@ -0,0 +1,78 @@
+/** 금아마스크WHITE */
+const kuma_white = 'https://smartstore.naver.com/kumaelectron/products/4922095600'
+/** 금아 블랙마스크 */
+const kuma_black = 'https://smartstore.naver.com/kumaelectron/products/4813999869'
+/** 금아덴탈마스크 */
+const kuma_dental = 'https://smartstore.naver.com/kumaelectron/products/4754238400'
+const test_product = 'https://smartstore.naver.com/kumaelectron/products/4836415470'
+const aer_mask = 'https://smartstore.naver.com/aer-shop/products/4722827602'
+
+/** 네이버 로그인 */
+const login = async (page, userId, password) => {
+  try {
+    await page.goto('https://www.naver.com')
+    await page.click('#account > a.link_login')
+    await page.waitForSelector('#id')
+
+    // await page.type('#id', userId)
+    // await page.type('#pw', password)
+    /* 네이버 CAPTCHA 무력화 */
+    await page.evaluate((id, pw) => {
+      document.querySelector('#id').value = id;
+      document.querySelector('#pw').value = pw;
+    }, userId, password)
+
+    await Promise.all([
+      page.click('#log\\.login'),
+      page.waitForNavigation({waitUntil: 'networkidle0'})
+    ])
+    await page.waitFor(2000)
+
+    // Check Login Success
+    const elementHandle = await page.$('iframe#minime');
+    const frame = await elementHandle.contentFrame();
+    await frame.waitForSelector('div.email');
+    const email = await frame.$eval('div.email', el => el.textContent);
+    console.log('Naver Login Suceess, %s', email)
+    return true
+  } catch (err) {
+    console.log('Naver Login Failed!!!')
+    console.log(err)
+    return false
+  }
+}
+
+/** 상품 구매 */
+const buyProduct = async (page) => {
+  // 옵션선택(for 아에르)
+  await page.select('select._combination_option', '14322412681')
+  // 구매하기 클릭
+  await Promise.all([
+    page.click('span.buy > a'),
+    page.waitForNavigation({waitUntil: 'networkidle0'})
+  ])
+
+  // 일반결제
+  await page.waitForSelector('#generalPayments')
+  await page.click('#generalPayments')
+  await page.waitForSelector('#pay1', {visible: true, timeout: 2000})
+  // 실시간계좌이체
+  await page.click('#pay1')
+  // 나중에결제
+  await page.click('#pay18')
+  // 전체 동의하기
+  await page.click('#allAgree')
+  await page.click('button.btn_payment')
+  await page.waitForNavigation({waitUntil: 'networkidle0'})
+
+  if (await page.$('.order_number')) {
+    const orderNo = await page.$eval('.order_number', el => el.textContent);
+    console.log('Order No : %s', orderNo)
+    return true
+  }
+  return false
+}
+
+module.exports.product = kuma_dental
+module.exports.login = login
+module.exports.buyProduct = buyProduct
